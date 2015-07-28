5f3028863cfcc16ec6eb60ce052d34590de05761
Ademola Plumptre <dplumptre@yahoo.com>
2015-07-28 10:30:45 +0100

    initial commit

--- FlashClass.php
+++ FlashClass.php
@@ -28,7 +28,7 @@ class FlashClass {
 This function set() uses two parameters
 the name of the css class or id 
 the second is the message you want to pass along
-note i am using bootstrap 3
+note i am using bootstrap 3 
 
 */    
 public static function  set($css,$message){
@@ -69,6 +69,7 @@ echo"<div class=\"alert ".Session::get('alert-class')."\">
 /*
 
 This functions displays errors
+
 for the form you are working on 
 
 normally you will have to ususally write 
@@ -82,7 +83,7 @@ echo"</div>";
  
  but you wont have to worry about that any more 
  
- because its all here
+ because its all here, check readme
 
 
 
